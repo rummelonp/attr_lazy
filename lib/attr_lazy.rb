cb6f156e42f32666ab3fc0757484c659967675b3
Kazuya Takeshima <mail@mitukiii.jp>
2013-03-01 01:29:58 +0900

    Create AttrLazy.

--- lib/attr_lazy.rb
+++ lib/attr_lazy.rb
@@ -1,5 +1,23 @@
-require "attr_lazy/version"
+# -*- coding: utf-8 -*-
+
+require 'attr_lazy/version'
 
 module AttrLazy
-  # Your code goes here...
+  def attr_lazy_reader(reader_name, &block)
+    define_method(reader_name) do
+      instance_variable_get("@#{reader_name}") ||
+        instance_variable_set("@#{reader_name}", instance_eval(&block))
+    end
+  end
+
+  def attr_lazy_accessor(accessor_name, &block)
+    attr_lazy_reader(accessor_name, &block)
+    define_method("#{accessor_name}=") do |value|
+      instance_variable_set("@#{accessor_name}", value)
+    end
+  end
+end
+
+class Module
+  include AttrLazy
 end
--- lib/attr_lazy/version.rb
+++ lib/attr_lazy/version.rb
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 module AttrLazy
-  VERSION = "0.0.1"
+  VERSION = '0.0.1'
 end
